1547e41174cb1952b3c626e5991dd5ac3e476d5b
kika88vk <69463446+kika88vk@users.noreply.github.com>
2023-08-18 17:01:10 +0300

    transfering fitching on App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,20 @@ import css from './App.module.css';
 import { Searchbar } from './Searchbar/Searchbar';
 import { ImageGallery } from './ImageGallery/ImageGallery';
 import { Modal } from 'components/Modal/Modal';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Loader } from 'components/Loader/Loader';
+import { apiImages } from 'ApiImages/ApiImages';
+import { Button } from 'components/Button/Button';
 
 export const App = () => {
   const [imageTags, setImageTags] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [bigImage, setBigImage] = useState({});
   const [page, setPage] = useState(1);
+  const [status, setStatus] = useState('idle');
+  const [error, setError] = useState(null);
+  const [totalHits, setTotalHits] = useState(0);
+  const [images, setImages] = useState(null);
 
   const handleFormSubmit = (imageTags, page) => {
     setImageTags(imageTags);
@@ -24,14 +31,55 @@ export const App = () => {
     setShowModal(false);
   };
 
+  useEffect(() => {
+    async function getImages() {
+      try {
+        setStatus('pending');
+        const photos = await apiImages(imageTags, page);
+        if (page === 1) {
+          setImages(photos.hits);
+          setStatus('resolved');
+        } else {
+          setImages(prevImages => [...prevImages, ...photos.hits]);
+
+          setStatus('resolved');
+        }
+
+        setTotalHits(photos.total);
+      } catch (error) {
+        setError(error.message);
+        setStatus('rejected');
+      }
+    }
+
+    if (imageTags !== '' && page > 0) {
+      getImages();
+    }
+    console.log('page', page);
+  }, [imageTags, page]);
+
+  const handleBtnChangePage = () => {
+    setPage(prevPage => prevPage + 1);
+    console.log('gallery page', page);
+  };
+
   return (
     <div className={css.App}>
       <Searchbar onPropSubmit={handleFormSubmit} />
-      <ImageGallery
-        imageTags={imageTags}
-        pageFromApp={page}
-        openModal={openModal}
-      />
+      {status === 'idle' && (
+        <h1 className={css.heading}>Enter what you're looking for</h1>
+      )}
+      {status === 'pending' && <Loader />}
+      {totalHits === 0 && status === 'resolved' && (
+        <h1 className={css.heading}>Sorry, pictures are not found!</h1>
+      )}
+      {status === 'resolved' && (
+        <ImageGallery imagesList={images} openModal={openModal} />
+      )}
+      {images?.length < totalHits / page && (
+        <Button onChangePage={handleBtnChangePage} />
+      )}
+      {status === 'rejected' && <h1 className={css.heading}>{error}</h1>}
       {showModal && (
         <Modal onClose={closeModal}>
           <img
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,68 +1,14 @@
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
 import css from './ImageGallery.module.css';
-import { useEffect, useState } from 'react';
-import { Loader } from 'components/Loader/Loader';
-import { Button } from 'components/Button/Button';
-import { apiImages } from 'ApiImages/ApiImages';
 import PropTypes from 'prop-types';
 
-export const ImageGallery = ({ imageTags, openModal, pageFromApp }) => {
-  const [images, setImages] = useState(null);
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState('idle');
-  const [page, setPage] = useState(pageFromApp);
-  const [totalHits, setTotalHits] = useState(0);
-
-  // useEffect(() => {
-  //   setPage(pageFromApp);
-  // }, [pageFromApp]);
-
-  useEffect(() => {
-    async function getImages() {
-      try {
-        setStatus('pending');
-        const photos = await apiImages(imageTags, page);
-        if (page === 1) {
-          setImages(photos.hits);
-          setStatus('resolved');
-        } else {
-          setImages(prevImages => [...prevImages, ...photos.hits]);
-
-          setStatus('resolved');
-        }
-
-        setTotalHits(photos.total);
-      } catch (error) {
-        setError(error.message);
-        setStatus('rejected');
-      }
-    }
-
-    if (imageTags !== '' && page > 0) {
-      getImages();
-    }
-    console.log('page', page);
-  }, [imageTags, page]);
-
-  const handleBtnChangePage = () => {
-    setPage(prevPage => prevPage + 1);
-    console.log('gallery page', page);
-  };
-
-  const showImages = Array.isArray(images) && images.length > 0;
+export const ImageGallery = ({ imagesList, openModal }) => {
+  const showImages = Array.isArray(imagesList) && imagesList.length > 0;
   return (
     <>
-      {status === 'idle' && (
-        <h1 className={css.heading}>Enter what you're looking for</h1>
-      )}
-      {status === 'pending' && <Loader />}
-      {totalHits === 0 && status === 'resolved' && (
-        <h1 className={css.heading}>Sorry, pictures are not found!</h1>
-      )}
-
-      {status === 'resolved' && showImages && (
+      {showImages && (
         <ul className={css.ImageGallery}>
-          {images.map(image => (
+          {imagesList.map(image => (
             <ImageGalleryItem
               key={image.id}
               bigPhoto={image.largeImageURL}
@@ -73,16 +19,11 @@ export const ImageGallery = ({ imageTags, openModal, pageFromApp }) => {
           ))}
         </ul>
       )}
-      {images?.length < totalHits / page && (
-        <Button onChangePage={handleBtnChangePage} />
-      )}
-      {status === 'rejected' && <h1 className={css.heading}>{error}</h1>}
     </>
   );
 };
 
 ImageGallery.propTypes = {
-  imageTags: PropTypes.string,
+  imagesList: PropTypes.array,
   openModal: PropTypes.func,
-  pageFromApp: PropTypes.number,
 };
